ec9beafd3b862af8dc4ed5b6f46c7168ad763d5c
carreuky <carreuky@gmail.com>
2023-01-29 23:47:37 +0300

    change on link

--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,7 @@ export const data = [
     image:
       "https://res.cloudinary.com/dodzzvz3z/image/upload/v1673354428/vibrant_gefha1.png",
     github: "https://github.com/carreuky/vibrant",
-    live: "vibrant-frontend.vercel.app",
+    live: "https://vibrant-frontend.vercel.app",
   },
   {
     id: 2,
@@ -27,7 +27,7 @@ export const data = [
     id: 4,
     name: "Job Seeker",
     image:
-      "https://res.cloudinary.com/dodzzvz3z/image/upload/v1673355952/Pics/job_at1r8m.png",
+    "https://res.cloudinary.com/dodzzvz3z/image/upload/v1673355952/Pics/job_at1r8m.png",
     github: "https://github.com/carreuky/Job-Portal",
     live: "https://job-portal-beige.vercel.app/",
   },
